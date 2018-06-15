f7c58e2b1e3f7331eb33d4edd64368534579df4c
albertocastano <bertocast@gmail.com>
2018-06-15 17:00:26 +0200

    Fix gitignore

--- upload_to_nserver.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-scp -r side-tests/*.py castano@156.35.105.23:/home/castano/development/experimentos/side-tests
-scp -r *.py castano@156.35.105.23:/home/castano/development/experimentos/
